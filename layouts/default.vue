b28e77cb4acddc5e8a5960184eade37330571693
Kiyo5hi <44930252+Kiyo5hi@users.noreply.github.com>
2023-06-17 04:27:20 +0000

    chore(*): initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex flex-col">
+    <header class="p-4 text-lg font-bold border-b">
+      UC Irvine Lecture Notes
+    </header>
+    <main class="p-4 flex-1">
+      <slot />
+    </main>
+  </div>
+</template>
