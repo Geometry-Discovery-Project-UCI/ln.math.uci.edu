6e198bd22e8020e27f4a3e59494f22b5d707e2c2
Kiyo5hi <44930252+Kiyo5hi@users.noreply.github.com>
2023-10-20 20:07:15 -0700

    chore(*): staged

--- content/index.md
+++ content/index.md
@@ -1,3 +1,7 @@
+---
+layout: home
+---
+
 # Home
 
 This website is a collection of my notes on various topics. I hope you find them useful.
---
+++ layouts/home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="w-screen p-4 gap-4 grid grid-flow-col auto-cols-auto">
+        <div class="card w-96 bg-base-100 shadow-xl" v-for="book in books">
+            <figure class="bg-base-300">
+                <div class="h-80 w-64 flex place-items-center text-center ">
+                    <div class="w-full">{{ book.title }}</div>
+                </div>
+            </figure>
+            <div class="card-body">
+                <h2 class="card-title">{{ book.title }}</h2>
+                <p>{{ book.description }}</p>
+                <div class="card-actions justify-end">
+                    <button class="btn btn-primary">Read</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+const { books } = await queryContent("/books").findOne();
+</script>
