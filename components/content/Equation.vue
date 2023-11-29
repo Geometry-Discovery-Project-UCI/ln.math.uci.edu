0f5251aa3395f755326714da4e3fd9439a1283c5
Kiyo5hi <44930252+Kiyo5hi@users.noreply.github.com>
2023-11-29 00:14:31 -0800

    feat(tags): added numbering to equations

---
+++ components/content/Equation.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="flex flex-row">
+        <div class="flex-1">
+            <slot></slot>
+        </div>
+        <div class="flex-none my-auto">({{ count }})</div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { useEquationCounterStore as useCounterStore } from "@/stores/counter";
+
+const counter = useCounterStore();
+counter.increment();
+const count = counter.count;
+
+withDefaults(defineProps<{
+    name?: string
+}>(), {
+    name: "Equation"
+});
+</script>
--- server/plugins/content.ts
+++ server/plugins/content.ts
@@ -2,6 +2,22 @@ export default defineNitroPlugin((nitroApp) => {
   nitroApp.hooks.hook('content:file:beforeParse', (file) => {
     if (file._id.endsWith('.md')) {
       file.body = file.body.replace(/\\(\[|\])/g, '$$$$')
+      file.body = file.body.replace(/\\\(((.|\n)*?)\\\)/g, `
+      ::Equation
+      $$$$
+      $1
+      $$$$
+      ::
+      `)
+      file.body = file.body.replace(/\\\{((.|\n)*?)\\\}/g, `
+      ::Equation
+      $$$$
+      \\begin{aligned}
+      $1
+      \\end{aligned}
+      $$$$
+      ::
+      `)
     }
   })
 })
